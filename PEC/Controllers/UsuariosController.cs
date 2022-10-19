81b556857670d580128e76e7ccf98a3bff9c9150
Julia Gomes Cassemiro <julia.cassemiro@vaxxinova.com.br>
2022-10-18 21:33:32 -0300

    ajustes api

--- PEC/Controllers/UsuariosController.cs
+++ PEC/Controllers/UsuariosController.cs
@@ -24,9 +24,6 @@ namespace PEC.Controllers
         }
 
 
-        // POST: api/Usuarios
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPost]
         public object GetIniciarSession(Usuarios_Repres UR)
         {
@@ -35,7 +32,10 @@ namespace PEC.Controllers
 
             if (login != null && login != "")
             {
-                var tbUsuarios = _context.usuario_Repres.Where(q => (login.Length <= 9 && q.ID_Repres == int.Parse(login.ToString())) || (login.Length > 9 && q.CPF_CNPJ == login.ToString())).Take(1).ToList();
+                //var teste = _context.usuario_Repres.Where(q => (login.Length <= 9 && q.ID_Repres == int.Parse(login.ToString())) || (login.Length > 9 && q.CPF_CNPJ == login.ToString())).Take(1).ToList();
+                var tbUsuarios = _context.usuario_Repres.Where(q => ((bool)IsNumeric(login) && login.Length <= 9) ?
+                                    q.ID_Repres.ToString() == login : (q.CPF_CNPJ == login)).Take(1).ToList();
+
                 if (tbUsuarios.Count() != 0)
                 {
                     if (tbUsuarios.Single().NM_Senha == Classes.Cryptografia.Criptografia(senha, Classes.Cryptografia.Tipo_Operacao.Cifra))
@@ -65,63 +65,12 @@ namespace PEC.Controllers
             }
             else
                 return ("error4");
-
-            //    if (login != null && login != "")
-            //    {
-
-            //        var tbUsuarios = usuarios_Repres.Where(u => (login.Trim().Length <= 9 && u.ID_Repres == int.Parse(login.ToString()) || login.Trim().Length > 9 && u.CPF_CNPJ == login.ToString())).Take(1).ToList();
-            //        if (tbUsuarios.Count() != 0)
-            //        {
-            //            //if (tbUsuarios.Single().NM_Senha == Classes.Cryptografia.Criptografia(Autenticar.Password, Classes.Cryptografia.Tipo_Operacao.Cifra))
-            //            //{
-            //            var user = usuarios.Single(q => q.ID_Usuario == tbUsuarios.Single().ID_Usuario);
-
-            //            var authenticate = true;
-
-            //            if (authenticate)
-            //            {
-            //                return (user);
-            //            }
-            //            else
-            //            {
-            //                return ("error1");
-            //            }
-
-            //        }
-            //        else
-            //        {
-            //            return ("error2");
-            //        }
-            //    }
-            //    else
-            //    {
-            //        return ("error3");
-            //    }
-            //}
-            //var nome = u.NM_Usuario;
-            //var senha = u.NM_Senha;
-            //var Autenticado = false;
-            //try
-            //{
-            //    var ad = new DirectoryEntry("LDAP://biovetvaxxinova.vgp.local", nome, senha);
-            //    var nativeObject = ad.NativeObject;
-            //    Autenticado = true;
-            //    var user = _context.usuario.Where(u => u.NM_Usuario.Equals(nome)).ToList();
-
-            //    if (user == null)
-            //    {
-            //        return NotFound();
-            //    }
-
-            //    return user;
-            //}
-            //catch (Exception ex)
-            //{
-            //    return NotFound();
-            //}
         }
 
-
+        protected  IsNumeric(this string text)
+        {
+            return double.TryParse(text, out double test);
+        }
     }
 }
 
